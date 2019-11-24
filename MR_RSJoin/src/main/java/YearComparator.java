d8f3da245aa3fad28e0f031a9f6c59334f9b738a
esethuraman <elattp@gmail.com>
2019-11-24 15:03:33 -0500

    Sort comparator implemented

---
+++ MR_RSJoin/src/main/java/YearComparator.java
@@ -0,0 +1,19 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class YearComparator extends WritableComparator {
+
+    public YearComparator() {
+        super(Text.class, true);
+    }
+    
+    @Override
+    public int compare(WritableComparable first, WritableComparable second) {
+        String year1 = ((Text) first).toString().split("-")[1];
+        String year2 = ((Text) second).toString().split("-")[1];
+        return year2.compareTo(year1);
+
+    }
+}
